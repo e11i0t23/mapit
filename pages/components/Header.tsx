7ad1af079a0296565bc40ea4810fcfc38fb0e990
Elliot Powell <johnp8727@gmail.com>
2023-03-05 01:26:15 +0000

    initial headers bar

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+
 export default function Header() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <nav className="navbar navbar-expand">
+        <div className="container-fluid">
+          <ul className="navbar-nav navbar-expand">
+            <li className="nav-item me-3">
+              <a className="nav-link">Sign-In</a>
+            </li>
+            <li className="nav-item me-3">
+              <a className="nav-link">Register</a>
+            </li>
+            <li className="nav-item me-3">
+              <a className="nav-link">Logout</a>
+            </li>
+            <li className="nav-item me-3">
+              <a className="nav-link">Save</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
