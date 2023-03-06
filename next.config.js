cb29c292e2c989c1408a7472ed8ed4f38f90c599
Elliot Powell <johnp8727@gmail.com>
2023-03-06 10:27:39 +0000

    add headers

--- next.config.js
+++ next.config.js
@@ -4,3 +4,31 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; style-src 'self' 'unsafe-inline'  https://fonts.googleapis.com; form-action 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com  https://*.googleapis.com; connect-src 'self' https://maps.googleapis.com https://identitytoolkit.googleapis.com https://firestore.googleapis.com; img-src 'self' data: https://maps.googleapis.com https://maps.gstatic.com; base-uri 'self'; frame-ancestors 'self' https://*.elliot-powell.com; font-src 'self' https://fonts.gstatic.com;",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ]
+  },
+}
